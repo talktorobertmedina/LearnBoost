71d453415f069a932463e751b093893cf5a92973
Robert Medina <talktorobertmedina@gmail.com>
2023-11-10 13:56:35 -0500

    Add project files.

---
+++ PingPong/PingPong.cpp
@@ -0,0 +1,12 @@
+﻿// PingPong.cpp : Defines the entry point for the application.
+//
+
+#include "PingPong.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
